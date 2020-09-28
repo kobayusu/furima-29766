72a94380cc03795a88ee9e286615586639439fb3
kobayusu <cb3ysk@gmail.com>
2020-09-28 14:45:51 +0900

    新規登録できてカラムに情報を保存できる

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, :family_name, :first_name, :family_kana, :first_kana, :birth_day, presence: true
+  has_many :items
+  has_many :orders
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :nickname
+    validates :family_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'カタカナで記入してください' } do
+    validates :family_kana
+    validates :first_kana
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 2020_09_25_060746) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "nickname", default: "", null: false
     t.string "family_name", null: false
     t.string "first_name", null: false
     t.string "family_kana", null: false
