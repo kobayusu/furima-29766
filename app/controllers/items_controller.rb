99da8205b1ff53ef3d364c706d724ef45a175175
kobayusu <cb3ysk@gmail.com>
2020-10-02 12:48:53 +0900

    カテゴリーなどの一覧選択表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,12 +2,20 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
+    @items = Item.order("created_at DESC")
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else 
+      render :new
+    end
   end
 
   def destroy
@@ -25,6 +33,14 @@ class ItemsController < ApplicationController
   private
 
   def move_to_index
-    redirect_to action: :index unless user_singed_in?
+    redirect_to action: :index unless user_signed_in?
+  end
+
+  def item_params
+    params.require(:item).permit(
+      :name, :explanation, :price, :postage_id, 
+      :shipping_day_id, :condition_id, :user,
+      :category_id,:prefecture_id, :image
+    )
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :condition, :postage, :shipping_day, :prefecture
+  belongs_to :user
+  has_one :order
+
+  validates :name, :explanation, :price, :postage_id, 
+            :shipping_day_id, :condition_id, :category_id,
+            :prefecture_id, :user, :image, presence: true
   
+  validates :category_id, :condition_id, 
+            :postage_id, :shipping_day_id,
+            :prefecture_id, numericality: { other_than: 1 }
 end
