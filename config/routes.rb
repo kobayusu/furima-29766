89576f035f023566b62ccbebc63e1240f573afb7
kobayusu <cb3ysk@gmail.com>
2020-10-07 19:28:46 +0900

    詳細のルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, only:[:index ,:new, :create]
+  resources :items, only:[:index ,:new, :create, :destroy, :edit, :update, :show]
   post "/items/new"
+  get "/items/show"
 end
