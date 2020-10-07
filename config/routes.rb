db2354d185295435111540ff7623d4e571d4189c
kobayusu <cb3ysk@gmail.com>
2020-10-07 19:30:08 +0900

    ルーティング7つのアクションまとめ

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, only:[:index ,:new, :create, :destroy, :edit, :update, :show]
+  resources :items
   post "/items/new"
   get "/items/show"
 end
