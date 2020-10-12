8090e25faf8605f9c6a89dfac315a8f4093bd95a
kobayusu <cb3ysk@gmail.com>
2020-10-12 20:38:39 +0900

    クレジット決済関係実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,16 +2,34 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @order = Order.new
   end
   
   def new
-    @order = order.new
   end
 
   def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
   end
 
   private
+  def order_params
+    params.permit(:price,:post_number,:city,:prefecture_id,:address,:building,:phone_number,:order).marge(user_id: current_user.id, item_id: params[:item_id],token: params[:token])
+  end
 
-
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
+    Payjp::Charge.create(
+      amount: order_params[:price],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,8 @@ class Order < ApplicationRecord
   has_one :destination
   belongs_to :user
   belongs_to :item
+
+  attr_accessor :token
+  validates :token, presence: true
+  validates :price, presence: true
 end
