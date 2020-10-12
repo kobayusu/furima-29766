2b258dbbe2c5b5d9f2a62fb62ca11b11980855ed
kobayusu <cb3ysk@gmail.com>
2020-10-12 18:57:00 +0900

    購入する商品の情報を表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,17 @@
 class OrdersController < ApplicationController
 
   def index
+    @item = Item.find(params[:item_id])
+  end
+  
+  def new
+    @order = order.new
   end
 
   def create
   end
-  
+
+  private
+
+
 end
