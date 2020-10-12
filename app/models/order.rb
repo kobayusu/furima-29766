bbb9e8f4fdef3f8b0935ea53546099819481055d
kobayusu <cb3ysk@gmail.com>
2020-10-12 21:26:09 +0900

    バリデーション移動

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,8 +2,4 @@ class Order < ApplicationRecord
   has_one :destination
   belongs_to :user
   belongs_to :item
-
-  attr_accessor :token
-  validates :token, presence: true
-  validates :price, presence: true
 end
--- app/models/order_distination.rb
+++ app/models/order_distination.rb
@@ -1,3 +1,9 @@
 class OrderDistination
   include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :token, :post_number, :prifecture_id, :city, :address, :building, :phone_number
+
+  validates :token, :post_number, :prifecture_id, :city, :address, :building, :phone_number, presence: true
+  validates :post_number, format:{ with: /\A[0-9]{3}-[0-9]{4}\z/, message: '必ず（-）ハイフンを間に挟んでください'}
+  validates :phone_number, format:{ with: /\A\d{11}\z/ }
+  validates :prifecture_id, numericality: {other_than: 1, message: "該当するものを選んでください" }
 end
