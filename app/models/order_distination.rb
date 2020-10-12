87ab109962ce23128472f44b58311bf684c88009
kobayusu <cb3ysk@gmail.com>
2020-10-12 21:33:53 +0900

    order情報の保存設定

--- app/models/order_distination.rb
+++ app/models/order_distination.rb
@@ -6,4 +6,10 @@ class OrderDistination
   validates :post_number, format:{ with: /\A[0-9]{3}-[0-9]{4}\z/, message: '必ず（-）ハイフンを間に挟んでください'}
   validates :phone_number, format:{ with: /\A\d{11}\z/ }
   validates :prifecture_id, numericality: {other_than: 1, message: "該当するものを選んでください" }
+
+  def save
+    #オーダー情報を保存、order変数に代入
+    order = Order.create(user_id: user_id, item_id: item_id)
+    #配送先の情報を保存
+    Destination.create(post_number: post_number, prifecture_id: prifecture_id, city: city, address: address, building: building, phone_number: phone_number, order_id: order_id)
 end
